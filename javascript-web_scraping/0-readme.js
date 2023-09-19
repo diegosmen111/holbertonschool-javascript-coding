098ccba0f83e011173a87292025ba1cb94ffba2d
diegosmen111 <dbav_10@hotmail.com>
2023-09-19 11:41:32 -0700

    fix task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const args = process.argv;
-/***
- * implementation of readFile
- */
-fs.readFile(args[2], 'utf8', function (err, data) {
-  if (err) return console.error(err);
-  console.log(data.toString());
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
